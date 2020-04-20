fa122f512b8591dbf489019893a57bab70b89caf
Zoey Chesny <z.chesny@gmail.com>
2020-04-20 17:35:29 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,6 @@
 module Players
   class Computer < Player
-
+    # FIXME: improve logic to make Computer unbeatable 
     def move(board)
       for i in 1..9 do
         return i.to_s if board.valid_move?(i)
